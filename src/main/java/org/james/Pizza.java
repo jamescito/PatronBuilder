8776a15da384b3a1326fae5c02949040d6bad6d1
jamescito <jr76407900@gmail.com>
2021-10-16 11:20:43 -0600

    Agregando la clase pizza como la base del proyecto

---
+++ src/main/java/org/james/Pizza.java
@@ -0,0 +1,57 @@
+package org.james;
+
+public class Pizza {
+    private String nombre;
+    private String masa;
+    private String salsa;
+    private String ingredientes;
+
+    public Pizza()
+    {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMasa() {
+        return masa;
+    }
+
+    public void setMasa(String masa) {
+        this.masa = masa;
+    }
+
+    public String getSalsa() {
+        return salsa;
+    }
+
+    public void setSalsa(String salsa) {
+        this.salsa = salsa;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "nombre='" + nombre + '\'' +
+                ", masa='" + masa + '\'' +
+                ", salsa='" + salsa + '\'' +
+                ", ingredientes='" + ingredientes + '\'' +
+                '}';
+    }
+
+    
+}
